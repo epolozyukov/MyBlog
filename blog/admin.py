63ed75792ae961c1583652433779ec8db15e6aa4
Yevhen Poloziukov <yevhen.poloziukov@eleks.com>
2020-01-14 12:53:41 +0200

    Blog MVP

---
+++ blog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from blog.models import Post, Category
+from projects.models import Project
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Project, ProjectAdmin)
